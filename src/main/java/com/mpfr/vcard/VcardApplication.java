3e0bb2494523615e3fb60b93e244789667645517
Mikadifo <michaelpadillaheredia@hotmail.com>
2021-11-11 18:53:57 -0500

    inti

---
+++ src/main/java/com/mpfr/vcard/VcardApplication.java
@@ -0,0 +1,13 @@
+package com.mpfr.vcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VcardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mpfr/vcard/VcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpfr.vcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
