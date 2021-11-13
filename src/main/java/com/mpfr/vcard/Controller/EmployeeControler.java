23a985ba895c4c60772f86328c6c46522d70f248
Dormamud03 <paulsanunga@gmail.com>
2021-11-13 15:21:10 -0500

    add controler and appconfig

---
+++ src/main/java/com/mpfr/vcard/Controller/EmployeeControler.java
@@ -0,0 +1,19 @@
+package com.mpfr.vcard.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/index")
+public class EmployeeControler {
+
+    @GetMapping("/empleado")
+    public String ver(Model model) {
+        model.addAttribute("empleado");
+        model.addAttribute("titulo", "Lista de Empleados");
+        return "nomina/empleados";
+    }
+
+}
